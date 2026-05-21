eaa29c36711751d11ac7fb1354f770fb01715bb4
SAMEERSIDDIQUI19 <sam.a.sid19@gmail.com>
2026-05-22 00:38:22 +0500

    Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright:wght@300;400;500;600;700&family=Sofia+Sans:ital,wght@0,1..1000;1,1..1000&display=swap" rel="stylesheet" />
+        <link rel="canonical" href="https://www.designagartistry.com/" />
       </head>
       <body className="bg-body">
         <AppShell>{children}</AppShell>
